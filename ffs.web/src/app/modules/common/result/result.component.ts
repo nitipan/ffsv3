0f25a3291ce241b3e4557fdc296756ba0eef8545
nitipan <nitipan.p@hotmail.com>
2017-07-30 12:38:50 +0700

    add level 2 result

--- ffs.web/src/app/modules/common/result/result.component.ts
+++ ffs.web/src/app/modules/common/result/result.component.ts
@@ -10,6 +10,7 @@ export class ResultComponent implements OnInit {
 
   result: any;
 
+  level2Grid: any[];
 
   constructor(private eventService: EventService) {
 
@@ -18,7 +19,20 @@ export class ResultComponent implements OnInit {
   ngOnInit() {
     this.eventService.calculatedSubject.subscribe(r => {
       this.result = r;
-      console.log(this.result);
+
+      if (this.result.param.assessmentLevel == 2) {
+        this.level2Grid = [];
+        for (let i = 0; i < this.result.result.resultDataGrid.length; i += 4) {
+          this.level2Grid.push({
+            col1: this.result.result.resultDataGrid[i],
+            col2: this.result.result.resultDataGrid[i + 1],
+            col3: this.result.result.resultDataGrid[i + 2],
+            col4: this.result.result.resultDataGrid[i + 3],
+            result: (this.result.result.resultDataGrid[i + 3].toLowerCase().indexOf("not") == -1)
+          });
+        }
+        console.log(this.level2Grid);
+      }
 
     });
   }
