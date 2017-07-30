d76b1942286500423dac84f92484d3d5ee66018b
nitipan <nitipan.p@hotmail.com>
2017-07-30 10:26:17 +0700

    fixed angular-cli development issue

--- ffs.api/Startup.cs
+++ ffs.api/Startup.cs
@@ -14,38 +14,25 @@ namespace ffs.api
 {
     public class Startup
     {
-        //const string dbFile = "db.sdf";
 
         public void Configuration(IAppBuilder app)
         {
-            //if (!File.Exists(dbFile))
-            //{
-            //    var assembly = Assembly.GetExecutingAssembly();
-            //    var resourceName = "ffs.api." + dbFile;
 
-            //    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //    {
-            //        using (var fileStream = File.Create(dbFile))
-            //        {
-            //            stream.Seek(0, SeekOrigin.Begin);
-            //            stream.CopyTo(fileStream);
-            //        }
-            //    }
-            //}
-            //var args = Environment.GetCommandLineArgs();
-            //if (args.Length > 0 && !args[0].EndsWith("OwinHost.exe"))
-            //{
-                const string rootFolder = "public";
-                var fileSystem = new PhysicalFileSystem(rootFolder);
-                var options = new FileServerOptions
-                {
-                    EnableDefaultFiles = true,
+            const string rootFolder = "public";
 
-                    FileSystem = fileSystem
-                };
+            if (!Directory.Exists(rootFolder))
+                Directory.CreateDirectory(rootFolder);
 
-                app.UseFileServer(options);
-           // }
+            var fileSystem = new PhysicalFileSystem(rootFolder);
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+
+                FileSystem = fileSystem
+            };
+
+            app.UseFileServer(options);
+
             app.UseNancy();
             app.UseStageMarker(PipelineStage.MapHandler);
         }
