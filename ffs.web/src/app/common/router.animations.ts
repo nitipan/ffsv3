f3fd0ae1ce63638d48e8c51286aa719cc218a181
nitipan <nitipan.p@hotmail.com>
2017-07-30 23:47:49 +0700

    fixed animations, responsive

--- ffs.web/src/app/common/router.animations.ts
+++ ffs.web/src/app/common/router.animations.ts
@@ -7,14 +7,14 @@ export function routerTransition() {
 
 export function fadeInOut() {
   return trigger('routerTransition', [
-    state('void', style({ position: 'fixed', width: '100%' })),
-    state('*', style({ position: 'fixed', width: '100%' })),
+    state('void', style({ position: 'fixed' })),
+    state('*', style({})),
     transition(':enter', [   // :enter is alias to 'void => *'
       style({ opacity: 0 }),
       animate(400, style({ opacity: 1 }))
     ]),
     transition(':leave', [   // :leave is alias to '* => void'
-      animate(400, style({ opacity: 0 }))
+      animate(200, style({ opacity: 0, position: 'fixed' }))
     ])
   ]);
 }
