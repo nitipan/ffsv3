1b0b2e2a35afed0102bce8f56fd4d52604ede5a0
Nitipan Pompan <nitipan.p@hotmail.com>
2018-10-08 08:56:24 +0700

    fixed fire report

--- ffs.web/src/app/modules/fire-damage/fire-damage.component.ts
+++ ffs.web/src/app/modules/fire-damage/fire-damage.component.ts
@@ -39,12 +39,15 @@ export class FireDamageComponent extends ModuleBase implements OnInit, AfterView
 
   form: FormGroup;
   unit: Observable<IUnit>;
-
+  currentUnit: IUnit;
   assessmentLevel: any = 1;
 
   constructor(private http: Http, private cdRef: ChangeDetectorRef, eventService: EventService, private datePipe: DatePipe) {
     super(eventService);
     this.unit = this.moduleEvent.unit.asObservable();
+    this.unit.subscribe(u => {
+      this.currentUnit = u;
+    });
   }
 
   ngAfterViewInit(): void {
